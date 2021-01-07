bb11fb94d6a2eb010cc29ffa1b5074c611c91cf5
majimaken <kengeeler@gmail.com>
2021-01-07 16:50:25 +0100

    Completed solve funtion and tested results

--- working.py
+++ working.py
@@ -1,19 +1,40 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan  2 20:53:25 2021
+# Sudoku Solver
+# Instead of using a method like naive bayes, we use a backtracking method. 
+# The solver puts in a number for every "0" and continues putting numbers for the remaining "0".
+# If it fails at one point, the solver goes back one step and tries another number. 
 
-@author: kenge
-"""
+# Enter your sudoku in the following format.
+# This board is from https://dingo.sbs.arizona.edu/~sandiway/sudoku/examples.html in order to check result. 
 
-board = [[7,8,0,4,0,0,1,2,0],
-         [6,0,0,0,7,5,0,0,9],
-         [0,0,0,6,0,1,0,7,8],
-         [0,0,7,0,4,0,2,6,0],
-         [0,0,1,0,5,0,9,3,0],
-         [9,0,4,0,6,0,0,0,5],
-         [0,7,0,3,0,0,0,1,2],
-         [1,2,0,0,0,7,4,0,0],
-         [0,4,9,2,0,6,0,0,7]]
+board = [[2,0,0,3,0,0,0,0,0],
+        [8,0,4,0,6,2,0,0,3],
+        [0,1,3,8,0,0,2,0,0],
+        [0,0,0,0,2,0,3,9,0],
+        [5,0,7,0,0,0,6,2,1],
+        [0,3,2,0,0,6,0,0,0],
+        [0,2,0,0,0,9,1,4,0],
+        [6,0,1,2,5,0,8,0,9],
+        [0,0,0,0,0,1,0,0,2]]
+
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            # Call solve() again with new board
+            if solve(bo):
+                return True
+
+            # If solve() does not return True, we reset the value to 0
+            bo[row][col] = 0
+
+    return False
 
 def print_board(bo):
     for i in range(len(bo)):
@@ -35,9 +56,9 @@ def find_empty(bo):
         for j in range(len(bo[0])):
             if bo[i][j] == 0:
                 return (i, j) 
+    return None
             
 def valid(bo, num, pos):
-    
     # Check row
     for i in range(len(bo[0])):
         # Check each element in row if it is the same
@@ -57,9 +78,26 @@ def valid(bo, num, pos):
     # (0,0), (0,1), (0,2)
     # (1,0), (1,1), (1,2)
     # (2,0), (2,1), (2,2)
+    # Using the pos (e.g. pos = (0,9)), we determine the index of the 3x3 boxes.
     box_x = pos[1] // 3
     box_y = pos[0] // 3
     
+    #Check if we have same number in 3x3 box.
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if bo[i][j] == num and (i, j) != pos:
+                return False
+    
+    return True
+
+
+print_board(board)
+solve(board)
+print("_______________________\n")
+print_board(board)
+
+
+
   
     
             
